ed1792a429b77b374b48b7dcc44b644b599ea4f4
Nikhil Joshi <nikhil.ashok.joshi@gmail.com>
2020-09-02 21:14:48 -0700

    adding muting

--- nikwrapper.sh
+++ nikwrapper.sh
@@ -2,4 +2,4 @@
 
 sleep 10
 export DBUS_SESSION_BUS_ADDRESS=$(cat /tmp/omxplayerdbus.${USER:-root})
-/usr/bin/python3 -u /home/pi/nik-presents/nik_presents.py --jsonfile /media/pi/Seagate\ Expansion\ Drive/media/np.json --mediadir /media/pi/Seagate\ Expansion\ Drive --verbose &> /home/pi/log.txt
+/usr/bin/python3 -u /home/pi/nik-presents/nik_presents_pi3.py --jsonfile /media/pi/Seagate\ Expansion\ Drive/media/np_pi3.json --mediadir /media/pi/Seagate\ Expansion\ Drive --verbose &> /home/pi/log.txt
