1e941b8fac2375d73b1521cce062e7015d144d08
Nikhil Joshi <nikhil.ashok.joshi@gmail.com>
2020-10-01 03:07:40 -0700

    adding comments

--- nikwrapper.sh
+++ nikwrapper.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 sleep 10
-#export DBUS_SESSION_BUS_ADDRESS=$(cat /tmp/omxplayerdbus.${USER:-root})
 /usr/bin/python3 -u /home/pi/nik-presents/nik_presents_pi3.py --jsonfile /media/pi/Seagate\ Expansion\ Drive/media/np_pi3.json --mediadir /media/pi/Seagate\ Expansion\ Drive --verbose &> /home/pi/log.txt
