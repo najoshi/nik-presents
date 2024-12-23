7e7ab67de8c04e91621a8e05d24e4a320a16173d
Nikhil Joshi <najoshi@ucdavis.edu>
2024-12-22 23:44:37 -0800

    testing on pi4

---
+++ pi4/nik_presents_pi4.py
@@ -0,0 +1,193 @@
+#!/usr/bin/python3
+
+import sys
+import random
+import json
+import argparse
+import os
+import threading
+
+import gi
+gi.require_version('Gtk', '4.0')
+from gi.repository import Gtk, Gdk
+from gtk4_mpv import MyRenderer
+
+
+class MainWindow(Gtk.ApplicationWindow):
+    def __init__(self, app, jsonfile, media_home, timeout, duration, verbose, *args, **kwargs):
+        super().__init__(application=app, *args, **kwargs)
+
+        # read in json track file
+        with open(jsonfile, 'r') as jfile:
+            data = jfile.read()
+
+        self.tracks = json.loads(data)["tracks"]
+        #shuffle the tracks so that they are random
+        # random.shuffle(self.tracks)
+
+        # set up CSS for styling widgets
+        css_provider = Gtk.CssProvider()
+        css_provider.load_from_path('/home/joshi/digital_media_frame_touch/nik-presents/pi4/style.css')
+        Gtk.StyleContext.add_provider_for_display(Gdk.Display.get_default(), css_provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
+
+        self.set_css_classes(['appbg'])
+
+        self.box = None
+        self.image = None
+        self.renderer = None
+        self.label = None
+        self.image_timer = None
+        self.track_number = -1
+        self.verbose = verbose
+        self.media_home = media_home
+        self.duration = duration
+
+        # set up key press 'q' for quitting
+        evk_quit = Gtk.EventControllerKey.new()
+        evk_quit.connect("key-pressed", self.key_press)
+        self.add_controller(evk_quit)
+
+        self.fullscreen()
+        self.next_track()
+
+
+    def key_press(self, event, keyval, keycode, state):
+        # check if keypress is 'q'
+        if keyval == Gdk.KEY_q:
+            if self.image_timer:
+                self.image_timer.cancel()
+            self.close()
+
+
+    def next_track(self):
+
+        if self.image_timer:
+            print("in timer cancel")
+            self.image_timer.cancel()
+
+        # if self.renderer:
+        #     self.renderer._mpv.terminate()
+
+        # increment the track number by 1
+        # if at end of tracks, then shuffle tracks and
+        # set track number to first track
+        self.track_number += 1
+        if self.track_number == len(self.tracks):
+            random.shuffle(self.tracks)
+            self.track_number = 0
+            
+        self.current_track = self.tracks[self.track_number]
+
+        if self.verbose:
+            print("playing track "+str(self.track_number)+": "+self.current_track['location'], flush=True)
+
+        if (self.current_track["type"] == "image"):
+            self.process_image()
+        elif (self.current_track["type"] == "video"):
+            self.play_video()
+
+
+    def process_image(self):
+        self.image = Gtk.Picture.new_for_filename(self.complete_path(self.current_track["location"]))
+
+        # get the width of the image
+        image_width = self.image.get_paintable().get_intrinsic_width()
+
+        label_text = ""
+        if image_width < 1800:
+            label_text = self.current_track["trip-text"] + "\n\n" + self.current_track["annot-text"]
+        else:
+            label_text = self.current_track["trip-text"] + " - " + self.current_track["annot-text"]
+
+        self.box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+        self.box.set_css_classes(['appbg'])
+
+        self.label = Gtk.Label(label=label_text)
+        # set label size based on centering the image
+        self.label.set_size_request((1920 - image_width) / 2, -1)
+        self.label.set_wrap(True)
+        self.label.set_css_classes(['annot'])
+        self.label.set_xalign(0)
+        self.label.set_yalign(0)
+
+        self.box.append(self.label)
+        self.box.append(self.image)
+        self.set_child(self.box)
+
+        # set a timer for the duration of the image
+        self.image_timer = threading.Timer(self.duration, self.next_track)
+        self.image_timer.start()
+
+
+    def play_video(self):
+        print("in play video")
+        # self.box.remove(self.image)
+        self.renderer = MyRenderer()
+        self.renderer.connect("realize", self.on_renderer_ready)
+        self.renderer._mpv.observe_property('eof-reached', self.handlePropertyChange)
+        self.set_child(self.renderer)
+        print("end of play video")
+
+    def on_renderer_ready(self, *_):
+        print("in renderer ready")
+        self.renderer.play(self.complete_path(self.current_track['location']))
+
+
+    def handlePropertyChange(self, name, value):
+        if self.verbose:
+            print('property change', name, value)
+
+        if (name == 'eof-reached' and value == True):
+            print('here')
+            del self.renderer
+            self.next_track()
+
+    def complete_path(self,track_file):
+        #  complete path of the filename of the selected entry
+        if track_file != '' and track_file[0]=="+":
+            track_file=self.media_home+track_file[1:]
+        return track_file
+    
+
+
+class MyApp(Gtk.Application):
+    def __init__(self, jsonfile, mediadir, timeout, duration, verbose):
+        super().__init__()
+        self.jsonfile = jsonfile
+        self.mediadir = mediadir
+        self.timeout = timeout
+        self.duration = duration
+        self.verbose = verbose
+        self.connect('activate', self.on_activate)
+
+    def on_activate(self, app):
+        self.win = MainWindow(app, self.jsonfile, self.mediadir, self.timeout, self.duration, self.verbose)
+        self.win.present()
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--jsonfile", help="JSON file with tracks.", required=True)
+parser.add_argument("--mediadir", help="Root directory for media.", required=True)
+parser.add_argument("--timeout", help="Number of seconds for PIR timeout (Default 120).", default=120, type=int)
+parser.add_argument("--duration", help="Number of seconds images are shown (Default 8).", default=8, type=int)
+parser.add_argument("--verbose", help="Informational output to STDOUT.", default=False, action='store_true')
+args = parser.parse_args()
+
+if (not os.path.exists(args.jsonfile)):
+    print("Error: JSON file '"+args.jsonfile+"' not found.", file=sys.stdout)
+    sys.exit(1)
+
+if (not os.path.isfile(args.jsonfile)):
+    print("Error: Path '"+args.jsonfile+"' is not a file.", file=sys.stdout)
+    sys.exit(1)
+    
+if (not os.path.exists(args.mediadir)):
+    print("Error: Media directory '"+args.mediadir+"' does not exist.", file=sys.stdout)
+    sys.exit(1)
+    
+if (not os.path.isdir(args.mediadir)):
+    print("Error: Media directory path '"+args.mediadir+"' is not a directory.", file=sys.stdout)
+    sys.exit(1)
+
+app = MyApp(args.jsonfile, args.mediadir, args.timeout, args.duration, args.verbose)
+app.run()
