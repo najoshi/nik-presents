5be7b0230ced1592c0a35b226328e73fd3a75ff0
Nikhil Joshi <najoshi@ucdavis.edu>
2025-04-21 00:52:14 -0700

    moving paused png to pi3 folder

--- nikwrapper.sh
+++ nikwrapper.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sleep 10
-/usr/bin/python3 -u /home/pi/nik-presents/nik_presents_pi3.py --jsonfile /media/pi/Seagate\ Expansion\ Drive1/media/np_pi3.json --mediadir /media/pi/Seagate\ Expansion\ Drive1 --verbose &> /home/pi/log.txt
+/usr/bin/python3 -u /home/pi/nik-presents/pi3/nik_presents_pi3.py --jsonfile /media/pi/Seagate\ Expansion\ Drive1/media/np_pi3.json --mediadir /media/pi/Seagate\ Expansion\ Drive1 --verbose &> /home/pi/log.txt
